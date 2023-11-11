961f5b2aee4689b8cedee9177dd14780728a730a
maximus7022 <pash6790@gmail.com>
2023-11-11 19:51:24 +0200

    Adding null resource to automate updating kubecfg + comments

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -1,3 +1,5 @@
+# ====================KMS KEY====================
+
 resource "aws_kms_key" "eks_kms_key" {
   description         = "KMS key for EKS"
   enable_key_rotation = true
@@ -19,6 +21,9 @@ data "aws_eks_cluster" "demo_cluster" {
 data "aws_eks_cluster_auth" "demo_cluster" {
   name = aws_eks_cluster.demo_cluster.name
 }
+
+
+# ====================EKS CLUSTER====================
 
 resource "aws_eks_cluster" "demo_cluster" {
   name     = var.cluster_name
@@ -42,15 +47,14 @@ resource "aws_eks_cluster" "demo_cluster" {
 
   tags = merge(var.tags, tomap({ "Name" = var.cluster_name }))
 
-  # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
-  # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
   depends_on = [
     aws_iam_role_policy_attachment.demo_AmazonEKSClusterPolicy,
     aws_iam_role_policy_attachment.demo_AmazonEKSVPCResourceController,
   ]
 }
 
-# NODE GROUP
+# ====================NODE GROUP====================
+
 resource "aws_eks_node_group" "demo_node" {
   cluster_name    = aws_eks_cluster.demo_cluster.name
   node_group_name = "demo_node_group"
@@ -68,8 +72,6 @@ resource "aws_eks_node_group" "demo_node" {
     max_unavailable = 2
   }
 
-  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
-  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
   depends_on = [
     aws_iam_role_policy_attachment.demo_node-AmazonEKSWorkerNodePolicy,
     aws_iam_role_policy_attachment.demo_node-AmazonEKS_CNI_Policy,
@@ -77,7 +79,8 @@ resource "aws_eks_node_group" "demo_node" {
   ]
 }
 
-#ADD-ONS
+# ====================ADD-ONS====================
+
 resource "aws_eks_addon" "vpc_cni" {
   cluster_name = aws_eks_cluster.demo_cluster.name
   addon_name   = "vpc-cni"
@@ -131,7 +134,8 @@ resource "aws_eks_addon" "kube_proxy" {
   ]
 }
 
-# ENSURING USE OF IAM ROLES INSIDE CLUSTER
+# ==========ENSURING USE OF IAM ROLES INSIDE CLUSTER==========
+
 data "tls_certificate" "demo_tls" {
   url = aws_eks_cluster.demo_cluster.identity[0].oidc[0].issuer
 
@@ -147,54 +151,13 @@ resource "aws_iam_openid_connect_provider" "demo_openid" {
   url             = aws_eks_cluster.demo_cluster.identity[0].oidc[0].issuer
 }
 
-#CLOUDWATCH SETUP
-resource "kubernetes_namespace" "aws_observability" {
-  metadata {
-    labels = {
-      aws-observability = "enabled"
-    }
-    name = "aws-observability"
+# ==========NULL RESOURCE FOR AUTOMATIC KUBECONFIG UPDATES==========
+
+resource "null_resource" "config_update" {
+  provisioner "local-exec" {
+    command = "aws eks update-kubeconfig --name ${var.cluster_name}"
   }
-
-  depends_on = [aws_eks_cluster.demo_cluster]
-}
-
-resource "kubernetes_config_map" "aws_observability" {
-  metadata {
-    name      = "aws-logging"
-    namespace = "aws-observability"
-  }
-
-  data = {
-    "filters.conf" = <<-EOT
-      [FILTER]
-        Name parser
-        Match *
-        Key_name log
-        Parser crio
-    EOT
-    "output.conf"  = <<-EOT
-      [OUTPUT]
-        Name cloudwatch_logs
-        Match   *
-        region us-east-1
-        log_group_name eks-pods-logs
-        log_stream_prefix from-eks-pods-
-        auto_create_group true
-        log_key log
-    EOT
-    "parsers.conf" = <<-EOT
-      [PARSER]
-        Name crio
-        Format Regex
-        Regex ^(?<time>[^ ]+) (?<stream>stdout|stderr) (?<logtag>P|F) (?<log>.*)$
-        Time_Key    time
-        Time_Format %Y-%m-%dT%H:%M:%S.%L%z
-    EOT
-  }
-
   depends_on = [
-    aws_eks_cluster.demo_cluster,
-    kubernetes_namespace.aws_observability
+    aws_eks_node_group.demo_node
   ]
 }
